fbbf54f07d82bb78e7d74619945639e0f813eddc
JinxedQuinn <146730606+JinxedQuinn@users.noreply.github.com>
2023-11-23 10:56:44 +0100

    ON A LE NOM FR CA TOURNE MAL EXPLICATION

--- testPokemon.js
+++ testPokemon.js
@@ -34,26 +34,28 @@ const getPokemon = (e) => {
     const type = document.querySelector("#type");
     const height = document.querySelector("#height")
 
-
-
-    fetch(`https://pokeapi.co/api/v2/pokemon/${name}`)
+    fetch(`https://pokebuildapi.fr/api/v1/pokemon/${name}`)
         .then((response) => response.json())
-        .then((data) => {
-            image.src = `${data.sprites.other['official-artwork'].front_default}`;
-            image.alt = `${data.name}`;
-            namepokemon.textContent = `${data.name.charAt(0).toUpperCase() + data.name.slice(1)}`;
-            number.textContent = `N° : ${data.id}`;
-            weight.textContent = `Weight : ${Math.round(data.weight/10)} kg`;
-            type.textContent = `Type : ${data.types[0].type.name.charAt(0).toUpperCase() + data.types[0].type.name.slice(1)}`
-            height.textContent = `Height : ${data.height*10} cm`
+        .then((frenchData) => {
+            const frenchName = frenchData.name;
+            namepokemon.textContent = frenchName.charAt(0).toUpperCase() + frenchName.slice(1);
 
-
+            fetch(`https://pokeapi.co/api/v2/pokemon/${frenchData.id}`)
+                .then((response) => response.json())
+                .then((data) => {
+                    image.src = `${data.sprites.other['official-artwork'].front_default}`;
+                    image.alt = `${data.name}`;
+                    number.textContent = `N° : ${data.id}`;
+                    weight.textContent = `Weight : ${Math.round(data.weight/10)} kg`;
+                    type.textContent = `Type : ${data.types[0].type.name.charAt(0).toUpperCase() + data.types[0].type.name.slice(1)}`
+                    height.textContent = `Height : ${data.height*10} cm`
+                });
         }).catch((err) => {
             alert('Pokemon not found', err);
         });
     e.preventDefault();
-    //prevents the default form submission behavior, ensuring that the page doesn't reload when the form is submitted.
 }
 document.querySelector('#search').addEventListener("click", getPokemon);
 
 //map(index) à voir
+
